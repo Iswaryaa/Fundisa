d3749e75fd8b269706bd016b0cc4f129b4cf3148
Iswaryaa <62008991+Iswaryaa@users.noreply.github.com>
2021-04-14 11:29:32 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-3 bg-inverse" style="position:sticky; top:100%;">
+        
+            <p class="m-0 text-center text-white">Copyright &copy; Fundisa - Sponser the education of a child</p>
+      
+        <!-- /.container -->
+    </footer>
---
+++ includes/header.php
@@ -0,0 +1,36 @@
+
+  
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+            <a class="navbar-brand" href="index.php">FUNDISA - SPONSOR THE EDUCATION OF A CHILD</a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin/index.php">Admin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="enroll-kid.php">Enrol kid</a>
+                    </li>
+                 
+                     <li class="nav-item">
+                        <a class="nav-link" href="sponsor-kid.php">Search Kids</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pindex.php">Sponsor Kids</a>
+                    </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact us</a>
+                    </li>
+                 
+                 
+                </ul>
+               
+            </div>
+           
+       
+        
+    </nav>
+   
